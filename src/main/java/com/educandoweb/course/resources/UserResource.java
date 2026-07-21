5a212f7a37bbeee89a217ce8117200c7f6e7a3c4
Vitor Campos <vitorlourencoc@gmail.com>
2026-07-21 19:26:21 -0300

    User entity and resource

---
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.educandoweb.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.educandoweb.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User user = new User(1L, "Vitor", "vitorlourencoc@gmail.com", "975713610", "123456");
+		return ResponseEntity.ok().body(user);
+	}
+
+}
