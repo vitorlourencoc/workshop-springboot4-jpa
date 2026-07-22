e65940885d25d991bb2bc8791c79571f9ca64c17
Vitor Campos <vitorlourencoc@gmail.com>
2026-07-22 18:21:58 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityReturnValueHandler;
-
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.services.UserService;
 
